6d09700e6c37de2144b1fe2f9b5b82a18d8534b8
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-05-31 17:31:58 -0400

    minor bug fix probably from a new pytorch version

--- utils/utils.py
+++ utils/utils.py
@@ -47,7 +47,7 @@ def accuracy(output, target, topk=(1, 5)):
 
         res = []
         for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
