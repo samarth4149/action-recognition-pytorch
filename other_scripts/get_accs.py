a4802bfd2039d857219e9c29174efae138cb990d
samarth4149 <samarth4149@gmail.com>
2022-06-07 19:02:58 -0400

    some changes in other_scripts

---
+++ other_scripts/get_accs.py
@@ -0,0 +1,18 @@
+from email import parser
+import numpy as np
+import torch
+import argparse
+from pathlib import Path
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Getting accs')
+    parser.add_argument('--snapshot_dir', type=str, default='snapshots_hmdb51_cls_split/tsn_kinetics_pt')
+    args = parser.parse_args()
+    
+    accs = []
+    for i in range(1, 11):
+        model_path = next(Path(args.snapshot_dir / f'split_{i}').glob('*')) / 'model_best.pth.tar'
+        curr_model = torch.load(model_path)
+        accs.append(curr_model['best_top1'])
+        
+    print(accs[::-1])
