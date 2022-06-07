1c68dcc06175428710e521114d8f6b0339c36835
samarth4149 <samarth4149@gmail.com>
2022-06-06 22:10:52 -0400

    added configs for hmdb_cls_splits

--- utils/dataset_config.py
+++ utils/dataset_config.py
@@ -1,67 +1,157 @@
 
 
 DATASET_CONFIG = {
-    'st2stv2': {
-        'num_classes': 174,
-        'train_list_name': 'train.txt',
-        'val_list_name': 'val.txt',
-        'test_list_name': 'test.txt',
-        'filename_seperator': " ",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 3
+    "st2stv2": {
+        "num_classes": 174,
+        "train_list_name": "train.txt",
+        "val_list_name": "val.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": " ",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 3
     },
-    'mini_st2stv2': {
-        'num_classes': 87,
-        'train_list_name': 'mini_train.txt',
-        'val_list_name': 'mini_val.txt',
-        'test_list_name': 'mini_test.txt',
-        'filename_seperator': " ",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 3
+    "mini_st2stv2": {
+        "num_classes": 87,
+        "train_list_name": "mini_train.txt",
+        "val_list_name": "mini_val.txt",
+        "test_list_name": "mini_test.txt",
+        "filename_seperator": " ",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 3
     },
-    'kinetics400': {
-        'num_classes': 400,
-        'train_list_name': 'train.txt',
-        'val_list_name': 'val.txt',
-        'test_list_name': 'test.txt',
-        'filename_seperator': ";",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 30
+    "kinetics400": {
+        "num_classes": 400,
+        "train_list_name": "train.txt",
+        "val_list_name": "val.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 30
     },
-    'mini_kinetics400': {
-        'num_classes': 200,
-        'train_list_name': 'mini_train.txt',
-        'val_list_name': 'mini_val.txt',
-        'test_list_name': 'mini_test.txt',
-        'filename_seperator': ";",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 30
+    "mini_kinetics400": {
+        "num_classes": 200,
+        "train_list_name": "mini_train.txt",
+        "val_list_name": "mini_val.txt",
+        "test_list_name": "mini_test.txt",
+        "filename_seperator": ";",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 30
     },
-    'moments': {
-        'num_classes': 339,
-        'train_list_name': 'train.txt',
-        'val_list_name': 'val.txt',
-        'filename_seperator': " ",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 0
+    "moments": {
+        "num_classes": 339,
+        "train_list_name": "train.txt",
+        "val_list_name": "val.txt",
+        "filename_seperator": " ",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
     },
-    'mini_moments': {
-        'num_classes': 200,
-        'train_list_name': 'mini_train.txt',
-        'val_list_name': 'mini_val.txt',
-        'filename_seperator': " ",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 0
+    "mini_moments": {
+        "num_classes": 200,
+        "train_list_name": "mini_train.txt",
+        "val_list_name": "mini_val.txt",
+        "filename_seperator": " ",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
     },
-    'hmdb51': {
-        'num_classes': 51,
-        'train_list_name': 'hmdb_train.txt',
-        'val_list_name': 'hmdb_val.txt',
-        'test_list_name': 'test.txt',
-        'filename_seperator': ";;",
-        'image_tmpl': '{:05d}.jpg',
-        'filter_video': 0
+    "hmdb51": {
+        "num_classes": 51,
+        "train_list_name": "hmdb_train.txt",
+        "val_list_name": "hmdb_val.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
     },
+    "hmdb51_cls_split_1": {
+        "num_classes": 48,
+        "train_list_name": "hmdb_train_cls_split_1.txt",
+        "val_list_name": "hmdb_val_cls_split_1.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_2": {
+        "num_classes": 45,
+        "train_list_name": "hmdb_train_cls_split_2.txt",
+        "val_list_name": "hmdb_val_cls_split_2.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_3": {
+        "num_classes": 42,
+        "train_list_name": "hmdb_train_cls_split_3.txt",
+        "val_list_name": "hmdb_val_cls_split_3.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_4": {
+        "num_classes": 39,
+        "train_list_name": "hmdb_train_cls_split_4.txt",
+        "val_list_name": "hmdb_val_cls_split_4.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_5": {
+        "num_classes": 36,
+        "train_list_name": "hmdb_train_cls_split_5.txt",
+        "val_list_name": "hmdb_val_cls_split_5.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_6": {
+        "num_classes": 33,
+        "train_list_name": "hmdb_train_cls_split_6.txt",
+        "val_list_name": "hmdb_val_cls_split_6.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_7": {
+        "num_classes": 30,
+        "train_list_name": "hmdb_train_cls_split_7.txt",
+        "val_list_name": "hmdb_val_cls_split_7.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_8": {
+        "num_classes": 27,
+        "train_list_name": "hmdb_train_cls_split_8.txt",
+        "val_list_name": "hmdb_val_cls_split_8.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_9": {
+        "num_classes": 24,
+        "train_list_name": "hmdb_train_cls_split_9.txt",
+        "val_list_name": "hmdb_val_cls_split_9.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    },
+    "hmdb51_cls_split_10": {
+        "num_classes": 21,
+        "train_list_name": "hmdb_train_cls_split_10.txt",
+        "val_list_name": "hmdb_val_cls_split_10.txt",
+        "test_list_name": "test.txt",
+        "filename_seperator": ";;",
+        "image_tmpl": "{:05d}.jpg",
+        "filter_video": 0
+    }
 }
 
 
