ce02182ae69025983b1d42b45cb55ebbda19ff37
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-08-15 19:01:27 -0400

    upping memory even further

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -119,7 +119,7 @@ def main1(args):
         addnl_params['mail_user'] = 'rpanda@ibm.com'
 
     executor.update_parameters(
-        mem_gb=40 * num_gpus_per_node,
+        mem_gb=60 * num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
         cpus_per_task=24,
         nodes=nodes,
